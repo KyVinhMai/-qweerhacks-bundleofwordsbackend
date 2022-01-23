fc919156c9426a3b6f110ae30297c9f3aa75126b
kyvinh mai <kyvinhmai@gmail.com>
2022-01-22 19:52:58 -0800

    Added affirmations and randomizer

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
-from flask import Flask
+from flask import Flask, jsonify # <-- sends dictionary back
 from flask_cors import CORS
-
-app = Flask(name)
-CORS(app)
+import random
 
 app = Flask(__name__)
+CORS(app)
+
+affirms = ["You are loved",
+           "Keep going ^-^",
+           "You are precious",
+           "Believe in yourself! ⧹(⦁ᴗ⦁)⧸",
+           "Your experiences are valid <3",
+           "!!! You Got this !!!",
+           "Life is valuable, and you are too!"
+           "You look like a snack （＾ω＾）"]
+
+randomaffirm = random.choice(affirms)
 
 @app.route('/')
 def index():
-    return "What's jiggy!"
+    return jsonify({"content": randomaffirm})
